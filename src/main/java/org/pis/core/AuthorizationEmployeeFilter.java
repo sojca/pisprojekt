228b2b808f4d26506079f6272f72a4919ebc7af7
Juraj Sojčák <juraj.sojcak@riganti.cz>
2018-04-23 16:35:02 +0200

    added authorization filters

---
+++ src/main/java/org/pis/core/AuthorizationEmployeeFilter.java
@@ -0,0 +1,36 @@
+package org.pis.core;
+
+import javax.inject.Inject;
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthorizationEmployeeFilter implements Filter {
+
+    @Inject
+    private AuthorizationBean authorizationBean;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        if(authorizationBean.isUserInRole("employee")){
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+        else{
+            String contextPath = ((HttpServletRequest) servletRequest).getContextPath();
+
+            HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+            httpResponse.sendRedirect(contextPath + "/403.xhtml");
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
